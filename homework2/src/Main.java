80964b3a272088a4c56b5fc7e6f3d25efcc7dacc
f7olivera <f7olivera@gmail.com>
2022-11-22 02:20:04 -0300

    Update main method

--- homework2/src/Main.java
+++ homework2/src/Main.java
@@ -1,32 +1,9 @@
-import applications.DesktopApp;
-import applications.MobileApp;
-import applications.Website;
-import people.*;
-
-import java.util.ArrayList;
+import applications.*;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer("John", "Doe", (short) 45, 41412);
-
-        // Create developer team
-        ArrayList<Developer> developers = new ArrayList<>();
-        developers.add(new Developer(124, 3000, "backend"));
-        developers.add(new Developer(234, 3000, "frontend"));
-        developers.add(new Developer(12, 3000, "full stack"));
-
-        // Create scrum master
-        ArrayList<ScrumMaster> scrumMasters = new ArrayList<>();
-        scrumMasters.add(new ScrumMaster(211,  3000));
-        scrumMasters.add(new ScrumMaster(210,  3000));
-
-        // Create product owners
-        ArrayList<ProductOwner> productOwners = new ArrayList<>();
-        productOwners.add(new ProductOwner(51, 5000));
-        productOwners.add(new ProductOwner(100, 5000));
-
         // Instantiate IT Company
-        ITCompany itCompany = new ITCompany("Solvd", developers, scrumMasters, productOwners);
+        ITCompany itCompany = new ITCompany("Solvd");
 
         /*
          * Requirements and quotation for a Website
