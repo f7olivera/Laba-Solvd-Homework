be81d349a3beb44f7f03237c02b0b723ea4894aa
f7olivera <f7olivera@gmail.com>
2022-11-22 11:14:40 -0300

    Add customerId

--- homework2/src/people/Customer.java
+++ homework2/src/people/Customer.java
@@ -1,15 +1,31 @@
 package people;
 
 public class Customer extends Person {
-    public Customer(String firstName, String lastName, short age, int id) {
-        super(firstName, lastName, age, id);
-    }
+    private int customerId;
 
     public Customer() {
         super();
     }
 
-    public Customer(int id) {
+    public Customer(int id, int customerId) {
         super(id);
+        this.customerId = customerId;
+    }
+
+    public Customer(String firstName, String lastName, short age, int id, int customerId) {
+        super(firstName, lastName, age, id);
+        this.customerId = customerId;
+    }
+
+
+    /*
+     * Getters and setters
+     */
+    public int getCustomerId() {
+        return this.customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
     }
 }
