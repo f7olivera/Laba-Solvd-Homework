6b418dc9aea4291a64d7f86592ce44453370e0c6
f7olivera <f7olivera@gmail.com>
2022-11-30 12:27:04 -0300

    Reorganize main

--- homework2/src/Main.java
+++ homework2/src/Main.java
@@ -1,3 +1,5 @@
+import company.ITCompany;
+import company.Quotation;
 import applications.*;
 import people.*;
 
@@ -5,10 +7,34 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-         * Instantiate IT Company
-         */
+        ITCompany itCompany = initCompany();
 
+        // Requirements and quotation for a Website
+        printExample(itCompany, new Website("Facebook", "www.facebook.com", 50000));
+
+        // Requirements and quotation for a MobileApp
+        printExample(itCompany, new MobileApp("WhatsApp", "android", 1000));
+
+        // Requirements and quotation for a DesktopApp
+        printExample(itCompany, new DesktopApp("Firefox", "linux"));
+    }
+
+    /**
+     * Print requirements and quotation for an Application
+     */
+    public static void printExample(ITCompany itCompany, Application app) {
+        System.out.printf("/*********** %s ***********/\n%n", app.getClass().getSimpleName());
+        String appRequirements = itCompany.getRequirements(app.getClass());
+        System.out.println(String.format("Requirements to build a %s: ", app.getClass().getSimpleName()) + appRequirements + "\n");
+
+        Quotation appQuotation = itCompany.getQuotation(app);
+        System.out.println("Quotation for this project:\n" + appQuotation);
+    }
+
+    /**
+     * Creates an instance of IT Company
+     */
+    public static ITCompany initCompany() {
         // Define base salaries for every worker
         HashMap<Class<? extends Worker>, Integer> baseSalaries = new HashMap<>();
         baseSalaries.put(Developer.class, 1000);
@@ -21,42 +47,6 @@ public class Main {
         appDetails.put(MobileApp.class, new AppDetails("name, operating systems to be supported and expected number of users", 7500));
         appDetails.put(DesktopApp.class, new AppDetails("name and platforms to be supported", 5000));
 
-        ITCompany itCompany = new ITCompany("Solvd", baseSalaries, appDetails);
-
-        /*
-         * Requirements and quotation for a Website
-         */
-        System.out.println("/*********** Website ***********/\n");
-        String requirements = itCompany.getRequirements(Website.class);
-        System.out.println("Requirements to build a website: " + requirements + "\n");
-
-        Website website = new Website("Facebook", "www.facebook.com", 50000);
-
-        Quotation quotation = itCompany.getQuotation(website);
-        System.out.println("Quotation for this project:\n" + quotation);
-
-        /*
-         * Requirements and quotation for a MobileApp
-         */
-        System.out.println("/*********** MobileApp ***********/\n");
-        String mobileAppRequirements = itCompany.getRequirements(MobileApp.class);
-        System.out.println("Requirements to build a mobile app: " + mobileAppRequirements + "\n");
-
-        MobileApp app = new MobileApp("WhatsApp", "android", 1000);
-
-        Quotation appQuotation = itCompany.getQuotation(app);
-        System.out.println("Quotation for this project:\n" + appQuotation);
-
-        /*
-         * Requirements and quotation for a DesktopApp
-         */
-        System.out.println("/*********** DesktopApp ***********/\n");
-        String desktopAppRequirements = itCompany.getRequirements(DesktopApp.class);
-        System.out.println("Requirements to build a desktop app: " + desktopAppRequirements + "\n");
-
-        DesktopApp desktopApp = new DesktopApp("Firefox", "linux");
-
-        Quotation desktopAppQuotation = itCompany.getQuotation(desktopApp);
-        System.out.println("Quotation for this project:\n" + desktopAppQuotation);
+        return new ITCompany("Solvd", baseSalaries, appDetails);
     }
 }
