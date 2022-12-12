0f78b0149944aa398211ead4d869c11601e22cd1
f7olivera <f7olivera@gmail.com>
2022-12-12 11:33:41 -0300

    Move first homework

---
+++ homework/src/main/java/com/solvd/homework1/Hello.java
@@ -0,0 +1,7 @@
+package com.solvd.homework1;
+
+public class Hello {
+    public final static void main(String []args) {
+        System.out.println("Hello " + (args.length > 0 ? args[0] : "World!"));
+    }
+}
