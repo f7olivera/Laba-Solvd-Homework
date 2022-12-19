2e137f75ef206f3a9789356fec94b4f78ce20ea4
f7olivera <f7olivera@gmail.com>
2022-12-19 11:31:06 -0300

    Move error message to constant

--- homework/src/main/java/com/solvd/itcompany/company/ProjectsManager.java
+++ homework/src/main/java/com/solvd/itcompany/company/ProjectsManager.java
@@ -4,16 +4,17 @@ import com.solvd.itcompany.exceptions.InvalidProjectStateException;
 import com.solvd.itcompany.exceptions.NoDevelopersException;
 import com.solvd.itcompany.exceptions.ProjectNotFoundException;
 import com.solvd.itcompany.interfaces.IDevelop;
-import com.solvd.itcompany.people.*;
+import com.solvd.itcompany.people.Team;
 
 import java.util.HashSet;
 
 public final class ProjectsManager implements IDevelop {
     private final HashSet<Project> projects = new HashSet<>();
+    private final String UNRECOGNIZED_PROJECT_MESSAGE = "This project does not belong to this company.";
 
     public void startProject(Project project) throws ProjectNotFoundException, NoDevelopersException, InvalidProjectStateException {
         if (!projects.contains(project)) {
-            throw new ProjectNotFoundException("This project does not belong to this company.");
+            throw new ProjectNotFoundException(UNRECOGNIZED_PROJECT_MESSAGE);
         } else if (project.getTeam().getDevelopers().isEmpty()) {
             throw new NoDevelopersException("Project can't start with no developers.");
         } else if (project.getState() != ProjectState.NOT_STARTED) {
@@ -27,12 +28,16 @@ public final class ProjectsManager implements IDevelop {
 
     public void finishProject(Project project) throws ProjectNotFoundException, InvalidProjectStateException {
         if (!projects.contains(project)) {
-            throw new ProjectNotFoundException("This project does not belong to this company.");
+            throw new ProjectNotFoundException(UNRECOGNIZED_PROJECT_MESSAGE);
         } else if (project.getState() != ProjectState.STARTED) {
-            String errorMessage =
-                    project.getState() == ProjectState.NOT_STARTED ?
-                            "This project has not started yet." :
-                            "This project is already finished.";
+            String errorMessage = "";
+            switch (project.getState()) {
+                case NOT_STARTED:
+                    errorMessage = "This project has not started yet.";
+                    break;
+                case FINISHED:
+                    errorMessage = "This project is already finished.";
+            }
 
             throw new InvalidProjectStateException(errorMessage);
         } else {
@@ -57,7 +62,7 @@ public final class ProjectsManager implements IDevelop {
 
     public Team removeProject(Project project) {
         if (!projects.contains(project)) {
-            throw new ProjectNotFoundException("This project does not belong to this company.");
+            throw new ProjectNotFoundException(UNRECOGNIZED_PROJECT_MESSAGE);
         } else {
             this.projects.remove(project);
             return project.getTeam();
