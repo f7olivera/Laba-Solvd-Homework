ccab2077618b9ec4dbfb100b57337910f9b96553
f7olivera <f7olivera@gmail.com>
2022-12-20 02:32:07 -0300

    Modify ProjectState enum

--- homework/src/main/java/com/solvd/itcompany/company/ProjectState.java
+++ homework/src/main/java/com/solvd/itcompany/company/ProjectState.java
@@ -1,7 +1,50 @@
 package com.solvd.itcompany.company;
 
+import java.time.Instant;
+
 enum ProjectState {
     NOT_STARTED,
-    STARTED,
-    FINISHED
+    STARTED(Instant.now().getEpochSecond()) {
+        @Override
+        public String toString() {
+            return "Project started at " + getTimestamp();
+        }
+    },
+    FINISHED(Instant.now().getEpochSecond(), -1) {
+        @Override
+        public String toString() {
+            return "Project finished at " + getTimestamp();
+        }
+    };
+
+    private long timestamp;
+    private int priority;
+
+    ProjectState() {
+    }
+
+    ProjectState(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    ProjectState(long timestamp, int priority) {
+        this(timestamp);
+        this.priority = priority;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
