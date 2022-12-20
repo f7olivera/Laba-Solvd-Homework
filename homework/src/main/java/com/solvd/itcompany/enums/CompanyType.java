e9e12996da9fb84029b6c995616153e9fee3051c
f7olivera <f7olivera@gmail.com>
2022-12-20 10:45:31 -0300

    Add field to enum

--- homework/src/main/java/com/solvd/itcompany/enums/CompanyType.java
+++ homework/src/main/java/com/solvd/itcompany/enums/CompanyType.java
@@ -1,8 +1,27 @@
 package com.solvd.itcompany.enums;
 
 public enum CompanyType {
-    SOLE_PROPRIETORSHIP,
-    CORPORATION,
-    LIMITED_LIABILITY_COMPANY,
-    COOPERATIVE;
+    SOLE_PROPRIETORSHIP("SP"),
+    CORPORATION("Corp"),
+    LIMITED_LIABILITY_COMPANY("LLC"),
+    COOPERATIVE("co-op");
+
+    private String abbreviation;
+
+    CompanyType(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    @Override
+    public String toString() {
+        return abbreviation;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
 }
