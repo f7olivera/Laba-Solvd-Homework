e9458aebab815f863d5193dc9bb455acca1ea4a9
f7olivera <f7olivera@gmail.com>
2022-12-30 17:32:02 -0300

    Formatting

--- homework/src/main/java/com/solvd/itcompany/interfaces/IFinance.java
+++ homework/src/main/java/com/solvd/itcompany/interfaces/IFinance.java
@@ -4,5 +4,6 @@ import com.solvd.itcompany.exceptions.NegativeAmountException;
 
 public interface IFinance {
     public void earn(int amount) throws NegativeAmountException;
+
     public void spend(int amount) throws NegativeAmountException;
 }
