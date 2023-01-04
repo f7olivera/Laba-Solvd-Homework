6239cf29519bbd683d113c7d64a04de7b951df24
f7olivera <f7olivera@gmail.com>
2023-01-04 18:05:29 -0300

    Formatting

--- homework/src/main/java/com/solvd/apache/tasks/Main.java
+++ homework/src/main/java/com/solvd/apache/tasks/Main.java
@@ -7,39 +7,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         WordCounter.countWords("src/main/resources/article.txt", "src/main/resources/article-count.txt");
 
-        /*
-         * SecondTask.createRandomFiles() example
-         */
+        // SecondTask.createRandomFiles() example
         // FilesManager.createRandomFiles(directoryPath, 5);
 
-        /*
-         * SecondTask.getLastModified() example
-         */
+        // SecondTask.getLastModified() example
         String directoryPath = "src/main/resources/random/";
 
         // Get last modified file
         System.out.print("Last modified file in " + directoryPath + ": ");
         System.out.println(SecondTask.getLastModified(directoryPath).getName());
 
-        /*
-         * SecondTask.renameFile() example
-         */
+        // SecondTask.renameFile() example
         try {
             SecondTask.renameFile("src/main/resources/random/original.txt", "new.txt");
         } catch (FileNotFoundException e) {
             SecondTask.renameFile("src/main/resources/random/new.txt", "original.txt");
         }
 
-        /*
-         * SecondTask.rotateWord() example
-         */
+        // SecondTask.rotateWord() example
         String wordToRotate = "text";
         System.out.printf("Rotating word \"%s\" ", wordToRotate);
         System.out.println(SecondTask.rotateWord("This is some random text (text).", wordToRotate, 1));
 
-        /*
-         * SecondTask.containsAny() example
-         */
+        // SecondTask.containsAny() example
         System.out.println(SecondTask.containsAny("Lorem ipsum dolor sit amet.", new String[]{"false"}));
         System.out.println(SecondTask.containsAny("Lorem ipsum dolor sit amet.", new String[]{"sit"}));
     }
