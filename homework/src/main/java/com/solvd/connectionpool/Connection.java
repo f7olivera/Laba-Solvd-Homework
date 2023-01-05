2d239cf2b32098ec53434ecb7bd24a953910777c
f7olivera <f7olivera@gmail.com>
2023-01-05 00:22:33 -0300

    Add id field

--- homework/src/main/java/com/solvd/connectionpool/Connection.java
+++ homework/src/main/java/com/solvd/connectionpool/Connection.java
@@ -1,4 +1,14 @@
 package com.solvd.connectionpool;
 
 public class Connection {
+    private final int id;
+
+    public Connection(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Connection " + id;
+    }
 }
