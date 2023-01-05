0df6b204b2ac0c7ab758e0a567e47228ad0fa960
f7olivera <f7olivera@gmail.com>
2023-01-05 00:22:04 -0300

    Add main method logic

--- homework/src/main/java/com/solvd/connectionpool/ClientsRunner.java
+++ homework/src/main/java/com/solvd/connectionpool/ClientsRunner.java
@@ -1,4 +1,27 @@
 package com.solvd.connectionpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ClientsRunner {
+    public static void main(String[] args) {
+        ConnectionPool connectionPool = new ConnectionPool();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        Client client1 = new Client(connectionPool, 1000);
+        Client client2 = new Client(connectionPool, 500);
+        Client client3 = new Client(connectionPool, 1500);
+        Client client4 = new Client(connectionPool, 2000);
+        Client client5 = new Client(connectionPool, 500);
+        executorService.execute(client1);
+        executorService.execute(client2);
+        executorService.execute(client3);
+        executorService.execute(client4);
+        executorService.execute(client5);
+
+        // All threads are executed using 5 threads
+        // 3 threads are used to execute the 6 threads
+        executorService.shutdown();
+    }
 }
