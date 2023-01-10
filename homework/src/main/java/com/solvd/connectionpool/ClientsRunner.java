dd0d958a25ba368ab947c5acf27ec400b5e545f9
f7olivera <f7olivera@gmail.com>
2023-01-09 23:22:54 -0300

    Run main in two different threads

--- homework/src/main/java/com/solvd/connectionpool/ClientsRunner.java
+++ homework/src/main/java/com/solvd/connectionpool/ClientsRunner.java
@@ -1,9 +1,22 @@
 package com.solvd.connectionpool;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.concurrent.*;
 
 public class ClientsRunner {
+    private final static Logger LOGGER = LogManager.getLogger(ClientsRunner.class);
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        LOGGER.info("Testing connection pool.");
+        run();
+
+        LOGGER.info("Testing connection pool on a separate thread.");
+        runUsingThread();
+    }
+
+    public static void run() throws ExecutionException, InterruptedException {
         ConnectionPool connectionPool = new ConnectionPool(5);
 
         ExecutorService executorService = Executors.newFixedThreadPool(5);
@@ -25,5 +38,21 @@ public class ClientsRunner {
         completable7.get();
 
         executorService.shutdown();
+    }
+
+    public static void runUsingThread() {
+        ThreadRunner threadRunner = new ThreadRunner();
+        threadRunner.start();
+    }
+
+    private static class ThreadRunner extends Thread {
+        @Override
+        public synchronized void start() {
+            try {
+                ClientsRunner.run();
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
